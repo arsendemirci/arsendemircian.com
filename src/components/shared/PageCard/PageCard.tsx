67214ed888b598f144897e41e0fd2dd7a88999c8
Arsen <demirci.arsen@gmail.com>
2024-06-18 19:18:04 +0400

    scrooll to top fix

--- src/components/shared/PageCard/PageCard.tsx
+++ src/components/shared/PageCard/PageCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, useRef } from 'react';
 import { AppContext } from '@/context/contextProvider';
 import styles from './PageCard.module.scss';
 import { PageCardProps } from '@types';
@@ -11,6 +11,7 @@ const PageCard: React.FC<PageCardProps> = ({ children }) => {
   const ctx = useContext(AppContext);
   const pathName = usePathname();
   const router = useRouter();
+  const divRef = useRef<any>();
   useEffect(() => {
     if (pathName !== ctx.currentPath) {
       setMainClass(`${styles.main} ${styles.close}`);
@@ -22,9 +23,15 @@ const PageCard: React.FC<PageCardProps> = ({ children }) => {
 
   useEffect(() => {
     setMainClass(`${styles.main} ${styles.open}`);
+    const { current } = divRef;
+    if (current !== null) {
+      current.scrollIntoView({ block: 'end', behavior: 'smooth' });
+    }
   }, [pathName]);
+
   return (
     <section className={mainClass}>
+      <div ref={divRef}></div>
       <div className={styles.wrapper}>
         <div className={styles.header}>
           {pathName == '/home' ? <HomeHeader /> : pathName.replace(/^\//, '')}
